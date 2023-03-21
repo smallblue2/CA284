890cb5585a4526580081779676e79537d34d560c
Niall <niall.ryan62@mail.dcu.ie>
2023-03-21 11:47:04 +0000

    Created a README.md

---
+++ README.d
@@ -0,0 +1,17 @@
+## Systems Programming (CA284)
+This repository was a self-exercise in git.
+My final grade in this course was 94%.
+
+## Learning Outcomes from this course
+This course was an introduction to C, and my first exploration into memory management and more.
+
+Through the learning of this course, I was able to;
+
+1. Apply problem-solving tecniques to writing well-structured programs in the C programming language.
+2. Recognise the relationship between programs expressed in C and in assembly code, including the implementation of expressions, control, procedures, and data structures.
+3. Demonstrate ability to understand basic intention of a program through its binary representation and apply these skills to debugging programs.
+4. Investigate the programmer’s interaction with the underlying system through the different APIs and abstractions.
+5. Analyse the consequences of imperfect system usage, such as poor memory and CPU performance, crashes, and security vulnerabilities.
+.6 Apply analytical abilities to create reliable and efficient programs exercising the different components of a modern computing system.
+
+
--- README.txt
+++
@@ -1,3 +0,0 @@
-College work from CA284 in DCU COMSCI2.
-
-Uploading as a self exercise with git.
